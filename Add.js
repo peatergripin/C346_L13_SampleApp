3adc5cb310b4e429649a45eb2f2a72dbaa6aed77
Irfan bin Imran <peatergripin@gmail.com>
2025-12-04 09:08:33 +0800

    Complete Lesson 13 Exercises

--- Add.js
+++ Add.js
@@ -1,35 +1,46 @@
-import React, {useState} from 'react';
-import {StatusBar, View, Button, Text, TextInput} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import {datasource} from './Data';
+import React, { useState } from "react";
+import { StatusBar, View, Button, Text, TextInput } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Add = ({navigation}) => {
-    const [letter, setLetter] = useState("");
-    const [type, setType] = useState("Vowels");
+const Add = ({ navigation, route }) => {
+  const [letter, setLetter] = useState("");
+  const [type, setType] = useState("Vowels");
 
-    return (
-        <View>
-            <StatusBar/>
-            <Text>Letter:</Text>
-            <TextInput maxLength={1} style={{borderWidth: 1}} onChangeText={(text) => setLetter(text)}/>
-            <Picker onValueChange={(value) => setType(value)}>
-                <Picker.Item label='Vowels' value='Vowels'/>
-                <Picker.Item label='Consonants' value='Consonants'/>
-            </Picker>
-            <Button title='Submit'
-                    onPress={() => {
-                        let item = {key: letter};
-                        let indexnum = 1;
-                        if (type == "Vowels") {
-                            indexnum = 0;
-                        }
-                        datasource[indexnum].data.push(item);
-                        navigation.navigate("Home")
-                    }
-                    }
-            />
-        </View>
-    );
+  const setData = async (value) => {
+    AsyncStorage.setItem("alphadata", value);
+    navigation.navigate("Home");
+  };
+
+  return (
+    <View>
+      <StatusBar />
+      <Text>Letter:</Text>
+      <TextInput
+        maxLength={1}
+        style={{ borderWidth: 1 }}
+        onChangeText={(text) => setLetter(text)}
+      />
+      <Picker onValueChange={(value) => setType(value)}>
+        <Picker.Item label="Vowels" value="Vowels" />
+        <Picker.Item label="Consonants" value="Consonants" />
+      </Picker>
+      <Button
+        title="Submit"
+        onPress={() => {
+          let mydata = JSON.parse(route.params.datastring);
+          let item = { key: letter };
+          let indexnum = 1;
+          if (type == "Vowels") {
+            indexnum = 0;
+          }
+          mydata[indexnum].data.push(item);
+          let stringdata = JSON.stringify(mydata);
+          setData(stringdata);
+        }}
+      />
+    </View>
+  );
 };
 
 export default Add;
--- Home.js
+++ Home.js
@@ -1,59 +1,86 @@
-import React from 'react';
-import {StatusBar, Button, SectionList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {datasource} from './Data';
+import React, { useState } from "react";
+import {
+  StatusBar,
+  Button,
+  SectionList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { datasource } from "./Data.js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 15,
-        margin: 10,
-        textAlign: 'left',
-    },
-    opacityStyle: {
-        borderWidth: 1,
-    },
-    headerText: {
-        fontSize: 20,
-        margin: 10,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontFamily: 'impact'
-    },
+  textStyle: {
+    fontSize: 15,
+    margin: 10,
+    textAlign: "left",
+  },
+  opacityStyle: {
+    borderWidth: 1,
+  },
+  headerText: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "impact",
+  },
 });
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
+  //Load the data here
+  const [mydata, setMydata] = useState([]);
+  const getData = async () => {
+    let datastr = await AsyncStorage.getItem("alphadata");
+    if (datastr != null) {
+      const jsondata = JSON.parse(datastr);
+      setMydata(jsondata);
+    } else {
+      setMydata(datasource);
+    }
+  };
+  getData();
 
-    const renderItem = ({item, index, section}) => {
-        return (
-            <TouchableOpacity style={styles.opacityStyle}
-                              onPress={() => {
-                                  navigation.navigate("Edit", {index: index, type: section.title, key: item.key})
-                              }
-                              }
-            >
-                <Text style={styles.textStyle}>{item.key}</Text>
-            </TouchableOpacity>
-        );
-    };
-
-    const sectionHeader = ({section: {title, bgcolor}}) => {
-        return (
-            <Text style={[styles.headerText, {backgroundColor: bgcolor}]}>
-                {title}
-            </Text>
-        );
-    };
+  const renderItem = ({ item, index, section }) => {
     return (
-        <View>
-            <StatusBar/>
-            <Button title='Add Letter' onPress={() => {
-                navigation.navigate("Add")
-            }}/>
-            <SectionList sections={datasource}
-                         renderItem={renderItem}
-                         renderSectionHeader={sectionHeader
-                         }/>
-        </View>
+      <TouchableOpacity
+        style={styles.opacityStyle}
+        onPress={() => {
+          navigation.navigate("Edit", {
+            index: index,
+            type: section.title,
+            key: item.key,
+          });
+        }}
+      >
+        <Text style={styles.textStyle}>{item.key}</Text>
+      </TouchableOpacity>
     );
+  };
+
+  return (
+    <View>
+      <StatusBar />
+      <Button
+        title="Add Letter"
+        onPress={() => {
+          let datastr = JSON.stringify(mydata);
+          navigation.navigate("Add", { datastring: datastr });
+        }}
+      />
+      <SectionList
+        sections={mydata}
+        renderItem={renderItem}
+        renderSectionHeader={({ section: { title, bgcolor } }) => (
+          <Text style={[styles.headerText, { backgroundColor: bgcolor }]}>
+            {title}
+          </Text>
+        )}
+      />
+    </View>
+  );
 };
 
 export default Home;
